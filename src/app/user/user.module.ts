5c6fabefc18e95fbf8324605264e3ef11d291c4d
AntonRusev <102323013+AntonRusev@users.noreply.github.com>
2022-11-22 17:10:26 +0200

    init

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './list/list.component';
+import { UserDetailComponent } from './detail/detail.component';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from './user-detail.guard';
+
+
+
+@NgModule({
+  declarations: [
+    UserListComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: 'user/list',
+        component: UserListComponent,
+      },
+      {
+        path: 'user/detail/:id',
+        canActivate: [AuthGuard],
+        component: UserDetailComponent,
+      },
+    ])
+  ],
+  exports: [
+    UserListComponent
+  ]
+})
+export class UserModule { }
